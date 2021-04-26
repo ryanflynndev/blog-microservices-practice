0cb2bef5c3e16db3d298f2317f1989c6e69a704a
Ryan Flynn <ryanflynn44@gmail.com>
2021-04-26 10:01:47 -0400

    Added routes for comments

--- comments/index.js
+++ comments/index.js
@@ -4,3 +4,15 @@ const { randomBytes } = require('crypto');
 
 const app = express();
 app.use(express.json());
+
+app.get('posts/:id/comments', (req, res) => {
+
+});
+
+app.post('posts/:id/comments', (req, res) => {
+    
+});
+
+app.listen(4001, () => {
+    console.log('Listening on port 4001!')
+})
